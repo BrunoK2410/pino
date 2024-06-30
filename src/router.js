143bfd5cfc7af7b1ccf8f06910d1309670b03654
BrunoK2410 <brunokatic2410@gmail.com>
2024-06-30 14:50:45 +0200

    new routes

--- src/router.js
+++ src/router.js
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "./pages/HomePage.vue";
 import AboutUsPage from "./pages/AboutUsPage.vue";
 import ContactPage from "./pages/ContactPage.vue";
-import ListPage from "./pages/animals/ListPage.vue";
+import ListPage from "./pages/ListPage.vue";
+import AnimalDetailsPage from "./pages/AnimalDetailsPage.vue";
 
 const routes = [
   { path: "/", component: HomePage },
@@ -27,10 +28,23 @@ const routes = [
     component: ListPage,
   },
   {
+    path: "/dogs/:slug",
+    name: "Dog details",
+    component: AnimalDetailsPage,
+    props: true,
+  },
+  {
     path: "/cats",
     name: "Cats",
     component: ListPage,
   },
+  {
+    path: "/cats/:slug",
+    name: "Cat details",
+    component: AnimalDetailsPage,
+    props: true,
+  },
+  { path: "/news", name: "News", component: ListPage },
 ];
 
 const router = createRouter({
